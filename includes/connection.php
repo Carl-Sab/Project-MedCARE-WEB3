a131ec09d9cfb5e34425213d6d290e069171e4af
RogerFahed <rogerfahed631@gmail.com>
2025-03-28 13:06:58 +0200

    building backend login

--- includes/connection.php
+++ includes/connection.php
@@ -1,2 +1,16 @@
 <?php
-$conn = new mysqli('localhost','root','','medcare');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "medcare";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+//------------------------------------------------------------------------------------------------------------
+//to check the connection if it work uncomment it and check if it succesfull when you are getting the database 
+//------------------------------------------------------------------------------------------------------------
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully!";
---
+++ public/view/doctorPanel.php
@@ -0,0 +1,3 @@
+<?php
+
+echo"doctorPanel page";
