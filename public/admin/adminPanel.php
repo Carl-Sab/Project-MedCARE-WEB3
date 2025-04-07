1c86a7d7c1227fc8fc651e0f46fedfb13345d565
RogerFahed <rogerfahed631@gmail.com>
2025-04-07 15:19:02 +0300

    updating admin panel

--- public/admin/adminPanel.php
+++ public/admin/adminPanel.php
@@ -17,10 +17,11 @@ $username = $_SESSION['Uname'];
   include "../../includes/header.php";
   echo "<style>.n2{display:flex}</style>";
   ?>
+  <br><br>
   <div class="container">
     <div class="card" id="dashboard">
-      <h2>Dashboard</h2>
-      <p>View reports, statistics, and manage platform activity.</p>
+      <h2>Statistics</h2>
+      <p>View statistics, and tracking payment and profit</p>
       <button><a href="adminDashboard.php">Go to Dashboard</a></button>
     </div>
 
@@ -37,16 +38,40 @@ $username = $_SESSION['Uname'];
     </div>
 
     <div class="card" id="chat">
-      <h2>Chat System</h2>
-      <p>Monitor chat logs and consultations.</p>
-      <button><a href="">View Chats</a></button>
+      <h2>Chat Review</h2>
+      <p>Monitor chat review and consultations.</p>
+      <button><a href="">Check Reviews</a></button>
     </div>
 
     <div class="card" id="appointments">
-      <h2>Doctor Booking</h2>
+      <h2>Doctor schedule </h2>
       <p>Track and manage doctor appointments.</p>
-      <button><a href="">Manage Appointments</a></button>
+      <button><a href="">Manage schedule</a></button>
     </div>
+    
+    <div class="card" id="userManage">
+    <h2>Manage Users</h2>
+    <p>Control user roles, permissions, and activity.</p>
+    <button><a href="#">Manage Users</a></button>
+  </div>
+
+  <div class="card" id="jobApply">
+    <h2>Job apply</h2>
+    <p>getting employement request accept/decline</p>
+    <button><a href="#">check requests</a></button>
+  </div>
+
+  <div class="card" id="Reports">
+    <h2>Reports</h2>
+    <p>tracking Reports and help users with their case</p>
+    <button><a href="#">check report</a></button>
+  </div>
+
+  <div class="card" id="Reports">
+    <h2>Test Result</h2>
+    <p>double check on test result for prevending problems</p>
+    <button><a href="#">check result</a></button>
+  </div>
   </div>
   <footer>
     
