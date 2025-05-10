3b50d52cd747927f8ec7b94d2b8276fec34ce393
Carl-Sab <carlsablouny02@gmail.com>
2025-05-10 14:21:18 +0300

    dr panel done

--- public/doctor/doctorStats.php
+++ public/doctor/doctorStats.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin Statistics</title>
+  <title>Doctor Statistics</title>
   <link rel="stylesheet" href="../css/adminStats.css">
 </head>
 <body>
