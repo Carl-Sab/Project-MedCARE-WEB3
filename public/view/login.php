e1e5ddc40cc13c3336f3a0190fad273daecf89d8
Carl-Sab <carlsablouny02@gmail.com>
2025-07-03 15:45:36 +0300

    Cookies done

--- public/view/login.php
+++ public/view/login.php
@@ -7,25 +7,26 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="../css/login.css">
-
 </head>
 <?php
 include "../../includes/connection.php";
 session_start();
+
 $msg = "";
+$remembered_username = isset($_COOKIE['username']) ? $_COOKIE['username'] : '';
 
 if (isset($_POST['Uname']) && isset($_POST['pass'])) {
     $uname = $_POST['Uname'];
     $pass = $_POST['pass'];
 
     // Prepare statement using MySQLi (object-oriented)
-    $stmt = $conn->prepare("SELECT * FROM users WHERE  user_name = ?");
+    $stmt = $conn->prepare("SELECT * FROM users WHERE user_name = ?");
     if ($stmt) {
         $stmt->bind_param("s", $uname); // "s" means string
         $stmt->execute();
 
         $result = $stmt->get_result();
-  
+
         if ($result && $result->num_rows > 0) {
             $row = $result->fetch_assoc();
 
@@ -33,6 +34,14 @@ if (isset($_POST['Uname']) && isset($_POST['pass'])) {
                 $_SESSION["id_user"] = $row['id_user'];
                 $_SESSION["Uname"] = $row['user_name'];
 
+                // Set cookie if remember me is checked
+                if (isset($_POST['remember'])) {
+                    setcookie("username", $uname, time() + (86400 * 30), "/"); // 30 days
+                } else {
+                    setcookie("username", "", time() - 3600, "/"); // Delete cookie
+                }
+
+                // Redirect based on role
                 if ($row['role'] == 'admin') {
                     header("Location: ../admin/adminPanel.php");
                 } else if ($row['role'] == 'doctor') {
@@ -64,39 +73,45 @@ if (isset($_POST['Uname']) && isset($_POST['pass'])) {
       <p>Login to your account</p>
 
       <form action="./login.php" method="POST">
+        <p style="color: red;"><?php echo (isset($msg) ? $msg : ""); ?></p>
+        
+        <input type="text" placeholder="Username" name="Uname" value="<?php echo htmlspecialchars($remembered_username); ?>" autocomplete="off" required>
 
-      <p style="color: red;"><?php echo (isset($msg)?$msg:"");?></p>
-        <input type="text" placeholder="Username" name="Uname" autocomplete="off" required>
         <div class="input-wrapper">
           <input type="password" id="password" placeholder="Password" name="pass" autocomplete="off" required>
           <button type="button" class="show-password" onclick="togglePassword()">
             <i class="fas fa-eye"></i>
           </button>
         </div>
+
+        <label style="display: flex; align-items: center; margin-top: 10px;">
+          <input type="checkbox" name="remember" <?php if(isset($_COOKIE['username'])) echo "checked"; ?>> Remember Me
+        </label>
+
         <a href="./forgotPass.php" class="forgot-password">Forgot Password?</a>
         <button id="login" type="submit">Log In</button>
       </form>
+
       <div class="no-account">
         Don't have an account? <a href="./signup.php" class="signup-link">Sign Up</a>
       </div>
     </div>
-    
   </div>
+</body>
 
-  </body>
-  <script>
-    function togglePassword() {
-      var password = document.getElementById("password");
-      var icon = document.querySelector(".show-password i");
-      if (password.type === "password") {
-        password.type = "text";
-        icon.classList.remove("fa-eye");
-        icon.classList.add("fa-eye-slash");
-      } else {
-        password.type = "password";
-        icon.classList.remove("fa-eye-slash");
-        icon.classList.add("fa-eye");
-      }
+<script>
+  function togglePassword() {
+    var password = document.getElementById("password");
+    var icon = document.querySelector(".show-password i");
+    if (password.type === "password") {
+      password.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      password.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
     }
-  </script>
+  }
+</script>
 </html>
